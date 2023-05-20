05219d33178627d675fb32d8e229a383083578f2
Doomslug <cbarrett17@georgefox.edu>
2023-05-20 11:13:40 -0700

    adjusted HumanPlayer.java functions

--- src/HumanPlayer.java
+++ src/HumanPlayer.java
@@ -8,34 +8,4 @@ public class HumanPlayer extends Player {
         super(name, color, true);
         this.scanner = new Scanner(System.in);
     }
-
-    @Override
-    public Move makeMove(Board board) {
-        System.out.println("It's " + getName() + "'s turn (" + getColor() + " pieces).");
-        System.out.println(board.toString());
-
-        while (true) {
-            System.out.print("Enter your move (e.g. 'e2 e4'): ");
-            String input = scanner.nextLine();
-
-            try {
-                String[] tokens = input.split(" ");
-                Spot source = new Spot(tokens[0]);
-                Spot target = new Spot(tokens[1]);
-                Move move = new Move(getColor(), source, target);
-                if (board.isValidMove(move, this)) {
-                    return move;
-                } else {
-                    System.out.println("Invalid move: " + input);
-                }
-            } catch (Exception e) {
-                System.out.println("Invalid input: " + input);
-            }
-        }
-    }
-
-    @Override
-    public boolean getForfeitStatus() {
-        return false;
-    }
 }
