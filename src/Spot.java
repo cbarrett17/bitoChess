132bb2f71a0f07d7e4d8ed6883b0af6d89a20e86
Doomslug <cbarrett17@georgefox.edu>
2023-05-19 20:53:26 -0700

    Added initial outline of Spot.java class

--- src/Spot.java
+++ src/Spot.java
@@ -1,3 +1,22 @@
 public class Spot {
-    // Implement class here
+    private int row;
+    private int col;
+    private Piece piece;
+    public Spot(int row, int col) {
+        this.row = row;
+        this.col = col;
+        this.piece = null;
+    }
+    public int getRow() {
+        return row;
+    }
+    public int getCol() {
+        return col;
+    }
+    public Piece getPiece() {
+        return piece;
+    }
+    public void setPiece(Piece piece) {
+        this.piece = piece;
+    }
 }
