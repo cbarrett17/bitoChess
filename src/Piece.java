1b93a40c08dd3c8fc16b6a841ea58a920e00421f
Doomslug <cbarrett17@georgefox.edu>
2023-05-20 15:23:46 -0700

    Piece.java subclasses created

--- src/Piece.java
+++ src/Piece.java
@@ -1,37 +1,169 @@
 import java.util.List;
 
 public abstract class Piece {
-    private String name;
-    private String color;
-    private Spot currentSpot;
-    private boolean hasMoved;
+    private final String _name;
+    private final String _color;
+    private Spot _currentSpot;
+    private boolean _hasMoved;
     public Piece(String name, String color, Spot currentSpot) {
-        this.name = name;
-        this.color = color;
-        this.currentSpot = currentSpot;
-        this.hasMoved = false;
+        this._name = name;
+        this._color = color;
+        this._currentSpot = currentSpot;
+        this._hasMoved = false;
     }
     public String getName() {
-        return name;
+        return _name;
     }
     public String getColor() {
-        return color;
+        return _color;
     }
     public Spot getCurrentSpot() {
-        return currentSpot;
+        return _currentSpot;
     }
     public void setCurrentSpot(Spot currentSpot) {
-        this.currentSpot = currentSpot;
+        this._currentSpot = currentSpot;
     }
     public boolean getHasMoved() {
-        return hasMoved;
+        return _hasMoved;
     }
     public void setHasMoved(boolean hasMoved) {
-        this.hasMoved = hasMoved;
+        this._hasMoved = hasMoved;
     }
     public abstract List<Spot> getLegalMoves(Board board);
     public abstract boolean move(Spot destinationSpot);
     public abstract boolean capture(Spot destinationSpot);
     // Additional methods specific to each piece type (e.g. castling for King,
     // promotion for Pawn) can be added as needed
+}
+
+class King extends Piece {
+
+    public King(String name, String color, Spot currentSpot) {
+        super(name, color, currentSpot);
+    }
+
+    @Override
+    public List<Spot> getLegalMoves(Board board) {
+        return null;
+    }
+
+    @Override
+    public boolean move(Spot destinationSpot) {
+        return false;
+    }
+
+    @Override
+    public boolean capture(Spot destinationSpot) {
+        return false;
+    }
+}
+
+class Queen extends Piece {
+
+    public Queen(String name, String color, Spot currentSpot) {
+        super(name, color, currentSpot);
+    }
+
+    @Override
+    public List<Spot> getLegalMoves(Board board) {
+        return null;
+    }
+
+    @Override
+    public boolean move(Spot destinationSpot) {
+        return false;
+    }
+
+    @Override
+    public boolean capture(Spot destinationSpot) {
+        return false;
+    }
+}
+
+class Rook extends Piece {
+
+    public Rook(String name, String color, Spot currentSpot) {
+        super(name, color, currentSpot);
+    }
+
+    @Override
+    public List<Spot> getLegalMoves(Board board) {
+        return null;
+    }
+
+    @Override
+    public boolean move(Spot destinationSpot) {
+        return false;
+    }
+
+    @Override
+    public boolean capture(Spot destinationSpot) {
+        return false;
+    }
+}
+
+class Bishop extends Piece {
+
+    public Bishop(String name, String color, Spot currentSpot) {
+        super(name, color, currentSpot);
+    }
+
+    @Override
+    public List<Spot> getLegalMoves(Board board) {
+        return null;
+    }
+
+    @Override
+    public boolean move(Spot destinationSpot) {
+        return false;
+    }
+
+    @Override
+    public boolean capture(Spot destinationSpot) {
+        return false;
+    }
+}
+
+class Knight extends Piece {
+
+    public Knight(String name, String color, Spot currentSpot) {
+        super(name, color, currentSpot);
+    }
+
+    @Override
+    public List<Spot> getLegalMoves(Board board) {
+        return null;
+    }
+
+    @Override
+    public boolean move(Spot destinationSpot) {
+        return false;
+    }
+
+    @Override
+    public boolean capture(Spot destinationSpot) {
+        return false;
+    }
+}
+
+class Pawn extends Piece {
+
+    public Pawn(String name, String color, Spot currentSpot) {
+        super(name, color, currentSpot);
+    }
+
+    @Override
+    public List<Spot> getLegalMoves(Board board) {
+        return null;
+    }
+
+    @Override
+    public boolean move(Spot destinationSpot) {
+        return false;
+    }
+
+    @Override
+    public boolean capture(Spot destinationSpot) {
+        return false;
+    }
 }
