b038def35506aec378668e8e51cfc34ce52895f2
Doomslug <cbarrett17@georgefox.edu>
2023-05-19 22:42:09 -0700

    added Move.java output *needs work*

--- src/Move.java
+++ src/Move.java
@@ -1,3 +1,32 @@
 public class Move {
-    // Implement class here
+    private String playerColor;
+    private Spot source;
+    private Spot target;
+    public Move(String playerColor, Spot source, Spot target) {
+        this.playerColor = playerColor;
+        this.source = source;
+        this.target = target;
+    }
+    public String getPlayerColor() {
+        return playerColor;
+    }
+    public void setPlayerColor(String playerColor) {
+        this.playerColor = playerColor;
+    }
+    public Spot getSource() {
+        return source;
+    }
+    public void setSource(Spot source) {
+        this.source = source;
+    }
+    public Spot getTarget() {
+        return target;
+    }
+    public void setTarget(Spot target) {
+        this.target = target;
+    }
+    @Override
+    public String toString() {
+        return playerColor + ": " + source.toString() + " -> " + target.toString();
+    }
 }
