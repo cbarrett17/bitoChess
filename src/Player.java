da4331601ac1443b99faac2d5fa421d6f3d55a68
Doomslug <cbarrett17@georgefox.edu>
2023-05-19 20:47:10 -0700

    Added initial outline of Player.java class

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,9 @@
-public class Player {
-    // Implement class here
+public abstract class Player {
+    private String name;
+    private String color;
+    private boolean isHuman;
+    // Additional properties and constructors as needed
+    public abstract boolean makeMove(Board board);
+    public abstract boolean getForfeitStatus();
+    // Additional methods as needed
 }
