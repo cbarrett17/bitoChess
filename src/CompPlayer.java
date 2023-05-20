6c510457ac54371ed13a81c20016f9e9d75a7134
Doomslug <cbarrett17@georgefox.edu>
2023-05-19 22:29:13 -0700

    added CompPlayer.java and Player.java output

--- src/CompPlayer.java
+++ src/CompPlayer.java
@@ -1,3 +1,37 @@
-public class CompPlayer {
-    // Implement class here
+import java.awt.Color;
+public class CompPlayer extends Player {
+    private String algorithm;
+    private int depth;
+    public CompPlayer(String name, Color color, String algorithm, int depth) {
+        super(name, color);
+        this.algorithm = algorithm;
+        this.depth = depth;
+    }
+    public String getAlgorithm() {
+        return algorithm;
+    }
+    public void setAlgorithm(String algorithm) {
+        this.algorithm = algorithm;
+    }
+    public int getDepth() {
+        return depth;
+    }
+    public void setDepth(int depth) {
+        this.depth = depth;
+    }
+    public Move getMove(Board board) {
+        // TODO: Implement the algorithm to calculate the computer player's move
+        // The algorithm should take into account the current state of the board, the computer player's color, and the depth of the search
+        return null;
+    }
+
+    @Override
+    public boolean makeMove(Board board) {
+        return false;
+    }
+
+    @Override
+    public boolean getForfeitStatus() {
+        return false;
+    }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,7 +1,14 @@
+import java.awt.*;
+
 public abstract class Player {
     private String name;
-    private String color;
+    private Color color;
     private boolean isHuman;
+
+    public Player(String name, Color color) {
+        // Implement constructor
+    }
+
     // Additional properties and constructors as needed
     public abstract boolean makeMove(Board board);
     public abstract boolean getForfeitStatus();
